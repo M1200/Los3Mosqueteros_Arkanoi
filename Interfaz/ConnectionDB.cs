7c647ce0726e3652d5cdc3b412e2f18b312eb516
DavidH29 <54407010+DavidH29@users.noreply.github.com>
2020-06-13 21:11:10 -0600

    Commit avance
    EPA: Los 3 Mosqueteros, 00198319, 00029916, 00156919

---
+++ Interfaz/ConnectionDB.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace Interfaz
+{
+    public class ConnectionDB
+    {
+        private static string cadenaC = "Server=127.0.0.1;" +
+                                        "Port=5432;" +
+                                        "UserId=postgres;" +
+                                        "Password=F9D17030.j;" +
+                                        "Database=arkanoid;";
+        
+        public static DataTable EjecutarConsulta(string consulta)
+        {
+            var conn = new NpgsqlConnection(cadenaC);
+            var ds = new DataSet();
+
+            conn.Open();
+            var da = new NpgsqlDataAdapter(consulta, conn);
+            da.Fill(ds);
+            conn.Close();
+
+            return ds.Tables[0];
+        }
+        
+        public static void EjecutarComando(string comando)
+        {
+            var conn = new NpgsqlConnection(cadenaC);
+
+            conn.Open();
+            var comm = new NpgsqlCommand(comando, conn);
+            comm.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+    }
+}
